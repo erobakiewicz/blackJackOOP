ef90d31e53b0f2ecbd1d9cfd560be5e9b34f0ac9
apoloniusz <erobakiewicz@gmail.com>
2023-03-15 15:17:29 +0100

    listClass

--- cards/class_approch.py
+++ cards/class_approch.py
@@ -1,3 +1,5 @@
+import random
+
 from cards.utils import Suit
 
 
@@ -23,3 +25,28 @@ class FaceCard(Card):
     def __init__(self, rank: int, suit: Suit) -> None:
         rank_str = {11: "J", 12: "Q", 13: "K"}[rank]
         super().__init__(rank=rank_str, suit=suit, hand=10, soft=10)
+
+
+def card(rank: int, suit: Suit) -> Card:
+    if rank == 1:
+        return AceCard("A", suit)
+    elif 2 <= rank < 11:
+        return Card(str(rank), suit, rank, rank)
+    elif 11 <= rank < 14:
+        return FaceCard(rank, suit)
+    raise Exception("Design Failure")
+
+
+class Deck:
+    def __init__(self) -> None:
+        self._cards = [card(r + 1, s) for r in range(13) for s in iter(Suit)]
+        random.shuffle(self._cards)
+
+    def pop(self) -> Card:
+        return self._cards.pop()
+
+
+class DeckList(list):
+    def __init__(self) -> None:
+        super().__init__(card(r + 1, s) for r in range(13) for s in iter(Suit))
+        random.shuffle(self)
